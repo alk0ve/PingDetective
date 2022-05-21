7e86e6f79ace15b9c7bc7427b0fde0191621877f
Alexei Kovelman <alexei.kovelman@gmail.com>
2022-05-21 15:41:03 +0300

    Initial solution structure

---
+++ TestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using libping;
+
+
+namespace TestConsole
+{
+    /*
+     * I'm using this class for quick and dirty testing.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
---
+++ libping/Ping.cs
@@ -0,0 +1,11 @@
+﻿namespace libping
+{
+    public class Ping
+    {
+        bool isAccessible(string hostnameOrIP)
+        {
+            return true;
+        }
+
+    }
+}
